7f97d23ddccc075083d51f621061f761f0df66fc
Yasuhisa Yoshida <syou6162@gmail.com>
2014-07-01 23:02:03 +0900

    ライブラリを追加

--- project.clj
+++ project.clj
@@ -4,6 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/data.json "0.2.5"]
+                 [org.clojure/tools.cli "0.2.1"]
                  [clj-time "0.6.0"]
-                 [org.clojure/tools.cli "0.2.1"]]
+                 [compojure "1.1.8"]
+                 [ring/ring-jetty-adapter "1.3.0"]]
   :main etymology-english.core)
