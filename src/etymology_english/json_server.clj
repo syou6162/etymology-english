666dc2f94e860ea21386c7b26e2b2399c96c9447
Yasuhisa Yoshida <syou6162@gmail.com>
2014-11-01 03:21:52 +0900

    語幹毎の単語リストを追加

--- src/etymology_english/json_server.clj
+++ src/etymology_english/json_server.clj
@@ -3,6 +3,7 @@
   (:require [compojure.handler :as handler])
   (:use [compojure.core :only (defroutes context GET PUT POST)])
   (:use [compojure.route :only (not-found files resources)])
+  (:use [hiccup.page-helpers :only (html5)])
   (:require [clojure.data.json :as json])
   (:use [etymology-english.core :only (root)]))
 
@@ -39,6 +40,34 @@
                    (sort-by second >)
                    (map first))]
     words))
+
+(defn get-word-stat-map []
+  (let [s (->> (vec (.list (java.io.File. logs-dir)))
+               (map #(str logs-dir "/" %))
+               (map slurp)
+               (clojure.string/join "\n"))
+        ;; 全単語を登場させるためにディフォルトで間違いのカウントを1つ追加しておく
+        default-count (->> root
+                           (map (fn [item]
+                                  (->> (:examples item)
+                                       (map (fn [w] [(:en w) "-"])))))
+                           (reduce into []))
+        result (->> (clojure.string/split s #"(\r)?\n")
+                    (remove (fn [line] (= line "")))
+                    (map #(clojure.string/split % #","))
+                    (into default-count)
+                    (group-by first)
+                    (map
+                     (fn [[w v]]
+                       (let [result (group-by second v)
+                             pos (count (get result "+" []))
+                             neg (count (get result "-" []))
+                             total (+ pos neg)]
+                         {w {:pos-cnt pos
+                             :neg-cnt neg
+                             :rate (/ pos total)}})))
+                    (reduce merge {}))]
+    result))
 
 (defn json-content []
   {"content" (->> (get-words)
@@ -83,7 +112,27 @@
              (fn [idx item]
                (str idx ": " (:en item) "\t" (:ja item) "\t"
                     (:root-en item) "\t" (:root-ja item))))
-            (clojure.string/join "<br />"))))
+            (clojure.string/join "<br />")))
+  (GET "/list-by-root" []
+       (let [stat-map (get-word-stat-map)
+             result (->> root
+                         (map
+                          (fn [w]
+                            [:li
+                             (str (:en w) " (" (:ja w) ")")
+                             [:ul
+                              (->> (:examples w)
+                                   (map
+                                    (fn [item]
+                                      [:li (:en item) " (" (:ja item) ") => "
+                                       (let [w (:en item)]
+                                         (str
+                                          "(total: " (int (* 100 (-> (get stat-map w) :rate))) "%"
+                                          ", correct: " (-> (get stat-map w) :pos-cnt)
+                                          ", wrong: " (-> (get stat-map w) :neg-cnt) ")"))])))]])))]
+         (html5
+          [:body
+           [:ul result]]))))
 
 (def app (handler/site routes))
 
